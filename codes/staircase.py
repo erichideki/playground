8c6272eed7aa991dba40cf2c73c53ade0951c116
erichideki <erichideki.is@gmail.com>
2021-11-09 22:41:56 -0300

    Update staircase description

--- codes/staircase.py
+++ codes/staircase.py
@@ -1,10 +1,4 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
+# https://www.hackerrank.com/challenges/staircase/problem
 
 def staircase(n):
     for i in range(1, n+1):
